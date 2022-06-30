493e7bfa77d38923bb4ed9afb356f6cbd11d692b
AlehMatiah <alexm.perez.argomedo@gmail.com>
2022-06-30 11:56:23 -0400

    Api JSON

--- sitio/serializers.py
+++ sitio/serializers.py
@@ -1,12 +1,12 @@
 from .models import *
 from rest_framework import serializers
 
-class ProductoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Producto
-        fields = '__all__'
-
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
+        fields = '__all__'
+class ProductoSerializer(serializers.ModelSerializer):
+    categoria = CategoriaSerializer(read_only=True)
+    class Meta:
+        model = Producto
         fields = '__all__'
